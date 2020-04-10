753aebd3881350c4b1cd0a8333321718e05a9dbb
ajhswan <andrewswan.dev@gmail.com>
2020-04-10 22:05:41 +0100

    Initialised passport middleware and setup passport config, and fixed issues wit syntax errors on passport.js, login.js and register.js - issues with the module.exports

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = keys.secretOrKey;
 
-modle.exports = passport => {
+module.exports = passport => {
     passport.use( new JwtStrategy(opts, (jwt_payload, done) => {
         User
             .findByID(jwt_payload.id)
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require('./routes/api/users');
 
 const app = express();
 
@@ -17,6 +20,13 @@ mongoose
     .then(() => console.log('MongoDB successfully connected'))
     .catch(err => console.log(err));
 
+//Passport middleware
+app.use(passport.initialize());
+
+//Passport config
+require('./config/passport')(passport);
+
+//set port, use Heroku server in cloud else use localhost:5000
 const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Server up and running on ${port} !`));
