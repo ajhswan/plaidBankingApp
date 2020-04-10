4024c7cec69d29ccec37012dccf649b3480d8ba2
ajhswan <andrewswan.dev@gmail.com>
2020-04-10 18:50:13 +0100

    updated keys.js with secretOrKey, and created passport.js for middleware authentication

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+    mongoURI: "mongodb+srv://aswan:StefHurry1@bankingapp-ktftq.mongodb.net/test",
+    secretOrKey: "secret"
+};
---
+++ config/passport.js
@@ -0,0 +1,24 @@
+const keys = require('../config/keys');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = keys.secretOrKey;
+
+modle.exports = passport => {
+    passport.use( new JwtStrategy(opts, (jwt_payload, done) => {
+        User
+            .findByID(jwt_payload.id)
+            .then(user => {
+                if (user) {
+                    return done(null, user);
+                }
+                return done(null, false);
+            })
+            .catch(err => console.log(err));
+    })
+    )
+};
