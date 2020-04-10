95eded8b0ebc73bf2e2b093927a1d91f38c7a647
ajhswan <andrewswan.dev@gmail.com>
2020-04-11 00:47:38 +0100

    removed keys.js file and added to .gitignore

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,4 @@
-const keys = require('../config/keys');
+const keys = require('./keys');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
